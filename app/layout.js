dbf25a9a85eb20595b7419c6f7287e75867094f7
zubair <zzubairahmed402@gmail.com>
2025-06-22 21:56:25 +0500

    My Own Website

--- app/layout.js
+++ app/layout.js
@@ -11,10 +11,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <body className={inter.className}>
-      <Navbar />
-      {children}
-    </body>
-  </html>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
   )
 }
